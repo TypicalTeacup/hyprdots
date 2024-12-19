f93ac80326ea1d505700e57588fb4ca957a016e1
TypicalTeacup <kskirel@wp.pl>
2024-12-19 16:41:31 +0100

    Shell config
    also swayidle

---
+++ .config/fish/conf.d/aliases.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    alias ls='eza --icons --group-directories-first'
+    alias la='ls -a'
+    alias ll='ls -al'
+
+    alias man='batman'
+    alias cat='bat'
+
+    alias v=nvim
+end
---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    starship init fish | source
+    zoxide init fish --cmd cd | source
+    fzf --fish | source
+end
+
+
+# Created by `pipx` on 2024-12-18 14:19:34
+set PATH $PATH /home/typicalteacup/.local/bin
